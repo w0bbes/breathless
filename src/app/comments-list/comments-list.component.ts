e9aae3ed9ca4bbca6cc3d25785482a5b557e77a1
Martijn@work <martijn@aliens.nl>
2016-11-07 13:40:37 +0100

    Sorting comments when we make the initial call

--- src/app/comments-list/comments-list.component.ts
+++ src/app/comments-list/comments-list.component.ts
@@ -34,7 +34,18 @@ export class CommentsListComponent implements OnInit, OnChanges {
 
         // call it and subscribe to it with the async pipe in the html
         this.comments$ = this.commentsService.comments$
-            .map(comments => comments.filter(comment => comment.topic_id === this.topic.id ));
+            .map(comments => comments.filter(comment => comment.topic_id === this.topic.id ))
+            .map(comments => {
+                comments.sort((a, b) => {
+
+                    let aDate: any = new Date(a.date);
+                    let bDate: any = new Date(b.date);
+
+                    return bDate - aDate;
+                });
+
+                return comments;
+            });
 
         // for the record, subscribe to get the amount of comments
         this.comments$.subscribe(comments => this.amountOfComments = comments.length);
